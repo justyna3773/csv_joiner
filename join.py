bd0f1d50fbefa6428750291408927610359502aa
justyna3773 <justyna3773@gmail.com>
2022-03-10 21:56:44 +0100

    Added csv file

--- join.py
+++ join.py
@@ -1,12 +1,19 @@
-from multiprocessing.sharedctypes import Value
+
 import join_with_pandas
 import sys
 
 if __name__ == "__main__":
     try:
-        join_class = join_with_pandas.csv_joiner(sys.argv[1], sys.argv[2],sys.argv[3], type=sys.argv[4] )
+        join_class = join_with_pandas.csv_joiner(
+            sys.argv[1], sys.argv[2], sys.argv[3], type=sys.argv[4])
+        join_class.join()
     except ValueError:
         raise("Value Error")
+    except FileNotFoundError:
+        raise("File not found\n")
+    except IndexError:
+        raise("Wrong number of arguments")
     if len(sys.argv) < 4:
         raise ValueError("Too few arguments\n")
-        sys.exit()
+    if sys.argv[4] not in ['left', 'right', 'inner']:
+        raise ValueError("Choose either left, right or inner join\n")
--- join_with_pandas.py
+++ join_with_pandas.py
@@ -12,19 +12,39 @@ class csv_joiner:
         self.rows2 = []
         self.headers2 = []
         self.col_name = col_name
-        self.df1 = pd.read_csv(self.file1)
-        self.df2 = pd.read_csv(self.file2)
+        self.df1 = pd.read_csv(self.file1, index_col=False)
+        self.df2 = pd.read_csv(self.file2, index_col=False)
         self.type = type
+
+    def join(self):
+        dataTypeObj1 = self.df1.dtypes
+        dataTypeObj2 = self.df2.dtypes
+        print(dataTypeObj1)
+        print(self.df1)
+        print(self.df2)
+        if dataTypeObj1[self.col_name] != dataTypeObj2[self.col_name]:
+            self.df2[self.col_name] = self.df2[self.col_name].astype(
+                dataTypeObj1[self.col_name])
+        print(self.df2.dtypes)
+        if self.type == "inner":
+            self.inner_join()
+        elif self.type == "left":
+            self.left_join()
+        elif self.type == "right":
+            self.right_join()
 
     def left_join(self):
         # assume that left is the first argument
         df3 = self.df1.merge(self.df2, on=[self.col_name], how='left')
+        print(df3)
         df3.to_csv("final.csv", index=False)
 
     def right_join(self):
-        df4 = self.df1.merge(self.df2, on=[self.col_name], how='right')
+        df4 = pd.merge(self.df1, self.df2, on=[self.col_name], how='right')
+        print(df4)
         df4.to_csv("final.csv", index=False)
 
     def inner_join(self):
-        df5 = self.df1.merge(self.df2, on=[self.col_name], how='inner')
+        df5 = pd.merge(self.df1, self.df2, on=[self.col_name], how='inner')
+        print(df5)
         df5.to_csv("final.csv", index=False)
